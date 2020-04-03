6458b668f9d8dfda36612e6efc962e902b979610
pushpendra kumar <pushpendra466@gmail.com>
2020-04-03 20:04:00 +0530

    Updates

---
+++ precache-manifest.fb4e868e96eb7f21925009302754c1cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2191f69342139d809661ec0718950c8f",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "9cd2dbea559b3c06c7f2",
+    "url": "/todo-react/static/css/main.246e00f6.chunk.css"
+  },
+  {
+    "revision": "dcb7a3bf5f6728014e7c",
+    "url": "/todo-react/static/js/2.190af892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.190af892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd2dbea559b3c06c7f2",
+    "url": "/todo-react/static/js/main.6d8b55e8.chunk.js"
+  },
+  {
+    "revision": "e5cc6c0f87aede2317e1",
+    "url": "/todo-react/static/js/runtime-main.60e92ffe.js"
+  }
+]);
